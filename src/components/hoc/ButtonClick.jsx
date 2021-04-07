78d2c1e37fdc46634354dc0f7ef59c7915bf292d
mbrsagor <mbrsagor@gmail.com>
2021-04-07 22:07:16 +0600

    HOC update v2

---
+++ src/components/hoc/ButtonClick.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ButtonClick extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ButtonClick
---
+++ src/components/hoc/withCounter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class withCounter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default withCounter
