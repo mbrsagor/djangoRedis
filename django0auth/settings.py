321436600856359c9bc48f358fddfebdb92510f2
mbrsagor <mbrsagor@gmail.com>
2021-08-04 23:22:34 +0600

    Refactor: Setting os path setup and rest-framework installation

--- django0auth/settings.py
+++ django0auth/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -43,6 +43,7 @@ LOCAL_APP = [
 
 THIRD_PARTY_APP = [
     'social_django',
+    'rest_framework',
 ]
 
 INSTALLED_APPS = DEFAULT_APPS + LOCAL_APP + THIRD_PARTY_APP
@@ -62,8 +63,7 @@ ROOT_URLCONF = 'django0auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'django0auth.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- feedapp/views.py
+++ feedapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework import views, status
+from rest_framework.response import Response
 
 
 def index(request):
