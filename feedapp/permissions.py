f572048fa3efcc2457ebe2fa7ae9a3a8510c370d
mbrsagor <mbrsagor@gmail.com>
2021-09-11 12:00:22 +0600

    New: Add new permissions

--- feedapp/permissions.py
+++ feedapp/permissions.py
@@ -17,3 +17,26 @@ class PermissionHelperMixin(object):
 
 class AuthPermission(object):
     pass
+
+
+class AuthorAllStaffAllButEditOrReadOnly(permissions.BasePermission):
+    edit_methods = ("PUT", "PATCH")
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if obj.author == request.user:
+            return True
+
+        if request.user.is_staff and request.method not in self.edit_methods:
+            return True
+
+        return False
